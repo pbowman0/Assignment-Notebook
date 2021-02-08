9636ced47b51772d569809d5211345801361ebe4
Paige Bowman <paigeelisabethbowman@gmail.com>
2021-02-08 10:42:00 -0600

    created custom struct in Content View and added four properties

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -14,6 +14,13 @@ struct ContentView: View {
     }
 }
 
+struct AssignmentItem: Identifiable, Codable {
+    var id = UUID()
+    var course = String()
+    var description = String()
+    var dueDate = Date()
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
